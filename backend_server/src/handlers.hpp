97aeeee5e007ba48f8f31a657421ee8f47af0884
Владислав Ушаков <ushakov_vl@list.ru>
2025-06-10 23:56:05 +0700

    Added endpoint for change username

--- backend_server/src/handlers.hpp
+++ backend_server/src/handlers.hpp
@@ -11,6 +11,7 @@
 #include "request_handlers/login_handler.hpp"
 #include "request_handlers/check_access_token_handler.hpp"
 #include "request_handlers/update_token_handler.hpp"
+#include "request_handlers/change_username_handler.hpp"
 
 
 void addHandlers(net::HTTPServer& server)
@@ -19,7 +20,8 @@ void addHandlers(net::HTTPServer& server)
 		std::make_pair("/signup", handlers::signup),
 		std::make_pair("/login", handlers::login),
 		std::make_pair("/check_access_token", handlers::check_access_token),
-		std::make_pair("/update_token", handlers::update_token)
+		std::make_pair("/update_token", handlers::update_token),
+		std::make_pair("/change_username", handlers::change_username)
 	};
 
 	for (unsigned int i = 0; i < handlers.size(); i++)
