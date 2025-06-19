62cf85ba85583d80fb18feae7bbfeca9d90a1407
Владислав Ушаков <ushakov_vl@list.ru>
2025-06-19 17:48:39 +0700

    Classes for header style moved to separate file

--- web_server/src/resources/chat/params/on_load.js
+++ web_server/src/resources/chat/params/on_load.js
@@ -3,9 +3,4 @@ check_token("http://127.0.0.1:8008/get_chat_data" + document.location.search, {m
 	console.log(data);
 	for (let i = 0; i < data.members.length; i++)
 		document.getElementById("chat-members").innerHTML += "<a>" + data.members[i].username + "</a>";
-
-	document.getElementById("main-header-chatname").innerHTML += data.name;
-	document.getElementById("main-header").innerHTML += (data.type == "group") ? "\
-	<div><button onclick=\"window.location.assign('/chat/params" + document.location.search + "');\"><a>Парамтеры</a></button></div>\
-	" : "";
 });
