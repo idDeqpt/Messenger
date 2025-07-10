a32fb2ef1d8ba8794fc9ec10ee4cb40df23ac978
Владислав Ушаков <ushakov_vl@list.ru>
2025-07-10 20:01:45 +0700

    Modified header of site

--- web_server/src/resources/js/display_header.js
+++ web_server/src/resources/js/display_header.js
@@ -1,12 +1,7 @@
 function display_header(title) {
 	document.getElementById("header").innerHTML = "\
-		<img src=\"/favicon.png\" alt=\"icon\">\
-		<div id=\"header-right-block\" class=\"header-right-block\">\
-			<div class=\"header-label\"><a>" + title + "</a></div>\
-			<div class=\"header-buttons\">\
-				<button onclick=\"window.location.assign('/profile')\"><a>Профиль</a></button>\
-				<button onclick=\"window.location.assign('/chats')\"><a>Чаты</a></button>\
-				<button onclick=\"exit();\"><a>Выйти</a></button>\
-			</div>\
-		</div>";
+		<button onclick=\"window.location.assign('/profile')\"><a>Профиль</a></button>\
+		<button onclick=\"window.location.assign('/chats')\"><a>Чаты</a></button>\
+		<button onclick=\"exit();\"><a>Выйти</a></button>\
+	";
 }
