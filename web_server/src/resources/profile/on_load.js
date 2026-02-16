392645170d1e39a2d67724a220298ed2a6ace396
idDeqpt <ushakov_vl@list.ru>
2026-02-16 23:03:46 +0700

    feat: move endpoint /profile to new style

--- web_server/src/resources/profile/on_load.js
+++ web_server/src/resources/profile/on_load.js
@@ -3,13 +3,13 @@ let token_payload = localStorage.getItem("access_token").split(".")[1];
 let self_page = params.get("id") == null;
 let user_id = (self_page) ? JSON.parse(atob(token_payload))["id"] : params.get("id");
 if (self_page)
-	document.getElementById("main-header").innerHTML += "<div class=\"main-header-buttons\"><button class=\"custom-button\" onclick=\"window.location.assign('/profile/params' + document.location.search);\"><a>Параметры</a></button></div>";
+	document.getElementById("main-content").innerHTML += "<div class=\"main-content-buttons\"><button class=\"custom-button\" onclick=\"window.location.assign('/profile/params' + document.location.search);\"><a>Параметры</a></button></div>";
 
 check_token("http://" + api_host + "/get_profile_data?id=" + user_id, {method: "GET"}, function(response){
 	response.json().then(data => {
 		console.log(data);
-		document.getElementById("main-header-info-photo").setAttribute("src", "data:img/png;base64, " + data.profile_photo_64);
-		document.getElementById("main-header-info-username").innerHTML = data.username;
-		document.getElementById("main-header-info-description").innerHTML = (data.description == "") ? "No description" : data.description;
+		document.getElementById("main-content-info-photo").setAttribute("src", "data:img/png;base64, " + data.profile_photo_64);
+		document.getElementById("main-content-info-username").innerHTML = data.username;
+		document.getElementById("main-content-info-description").innerHTML = (data.description == "") ? "No description" : data.description;
 	});
 });
